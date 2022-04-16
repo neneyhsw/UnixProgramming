3ed7b3dbd1f080231e5a6eb46ad69adecbb1a0a2
neneyhsw <63520696+neneyhsw@users.noreply.github.com>
2022-04-17 03:28:43 +0800

    Add files via upload

---
+++ hw2/logger.cpp
@@ -0,0 +1,83 @@
+#include <bits/stdc++.h>
+
+int chmod(const char *pathname, mode_t mode) {
+  return 0;
+}
+
+int chown(const char *pathname, uid_t owner, gid_t group) {
+  return 0;
+}
+
+int close(int fd) {
+  return 0;
+}
+
+int creat(const char *pathname, mode_t mode) {
+  return 0;
+}
+
+int creat64(const char *pathname, mode_t mode) {
+  return 0;
+}
+
+int fclose(FILE *stream) {
+  return 0;
+}
+
+FILE *fopen(const char *pathname, const char *mode) {
+  FILE *res;
+  return res;
+}
+
+FILE *fopen64(const char *pathname, const char *mode) {
+  FILE *res;
+  return res;
+}
+
+size_t fread(void *ptr, size_t size, size_t nmemb, FILE *stream) {
+  size_t res = 0;
+  return res;
+}
+
+
+size_t fwrite(const void *ptr, size_t size, size_t nmemb, FILE *stream) {
+  size_t res = 0;
+  return res;
+}
+
+int open(const char *pathname, int oflag, ...) {
+  return 0;
+}
+
+int open64(const char *pathname, int oflag, ...) {
+  return 0;
+}
+
+ssize_t read(int fd, void *buf, size_t count) {
+  ssize_t res = 0;
+  return res;
+}
+
+int remove(const char *pathname) {
+  return 0;
+}
+
+int rename(const char *old_path, const char *new_path) {
+  return 0;
+}
+
+FILE *tmpfile(void) {
+  FILE *res;
+  return res;
+}
+
+FILE *tmpfile64(void) {
+  FILE *res;
+  return res;
+}
+
+ssize_t write(int fd, const void *buf, size_t count) {
+  ssize_t res = 0;
+  return res;
+}
+
